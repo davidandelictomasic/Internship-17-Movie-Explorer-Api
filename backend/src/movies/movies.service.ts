a1abab83f2c3678d52ac939a71c289a878ff35a8
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-03-15 14:37:55 +0100

    Add GET and POST request logic

--- backend/src/movies/movies.service.ts
+++ backend/src/movies/movies.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.movie.findMany({
+      include: { genres: true },
+    });
+  }
+
+  findOne(id: number) {
+    return this.prisma.movie.findUnique({
+      where: { id },
+    });
+  }
+
+  create(dto: CreateMovieDto) {
+    return this.prisma.movie.create({
+      data: {
+        title: dto.title,
+        year: dto.year,
+        rating: dto.rating,
+        plot: dto.plot,
+        posterUrl: dto.posterUrl,
+        genres: {
+          connect: dto.genreIds.map((id) => ({ id })),
+        },
+      },
+      include: { genres: true },
+    });
+  }
+}
