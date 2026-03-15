d62ab45d762c05ab210684b7d71e8a9ac14908f9
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-03-15 16:45:27 +0100

    Add GET, POST and DELETE request logic for favorites

--- backend/src/favorites/favorites.service.ts
+++ backend/src/favorites/favorites.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateFavoriteDto } from './dto/create-favorite.dto';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.favorite.findMany({
+      include: { movie: true },
+    });
+  }
+
+  create(dto: CreateFavoriteDto) {
+    return this.prisma.favorite.create({
+      data: {
+        movieId: dto.movieId,
+      },
+      include: { movie: true },
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.favorite.delete({
+      where: { id },
+    });
+  }
+}
