c849bb2d3d000b0035e2f2ebc3b98b4ed3069a22
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-03-15 15:56:36 +0100

    Add GET and POST request logic for genres

--- backend/src/genres/dto/create-genre.dto.ts
+++ backend/src/genres/dto/create-genre.dto.ts
@@ -3,8 +3,8 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateGenreDto {
   @ApiProperty({ example: 'Action' })
   name: string;
-  @ApiProperty({ example: [1, 2] })
-  movieIds: number[];
+  @ApiProperty({ example: [1, 2], required: false })
+  movieIds?: number[];
 }
 
     
--- backend/src/genres/genres.service.ts
+++ backend/src/genres/genres.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateGenreDto } from './dto/create-genre.dto';
 
 @Injectable()
-export class GenresService {}
+export class GenresService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.genre.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.genre.findUnique({
+      where: { id },
+    });
+  }
+
+  create(dto: CreateGenreDto) {
+    return this.prisma.genre.create({
+      data: {
+        name: dto.name,
+        movies: dto.movieIds
+          ? { connect: dto.movieIds.map((id) => ({ id })) }
+          : undefined,
+      },
+    });
+  }
+}
