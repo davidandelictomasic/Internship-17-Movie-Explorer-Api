e0d4a33c3c695ec4215eff247988e9e8c83bf655
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-03-15 16:34:57 +0100

    Add GET, POST and DELETE request for favorites

--- backend/src/favorites/favorites.controller.ts
+++ backend/src/favorites/favorites.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
+import { FavoritesService } from './favorites.service';
+import { CreateFavoriteDto } from './dto/create-favorite.dto';
 
 @Controller('favorites')
-export class FavoritesController {}
+export class FavoritesController {
+  constructor(private readonly favoritesService: FavoritesService) {}
+
+  @Get()
+  findAll() {
+    return this.favoritesService.findAll();
+  }
+
+  @Post()
+  create(@Body() dto: CreateFavoriteDto) {
+    return this.favoritesService.create(dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.favoritesService.remove(id);
+  }
+}
