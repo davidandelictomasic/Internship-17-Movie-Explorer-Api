cf7126b42e81844b62b764e808b4e193cf9274b6
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-03-15 12:34:28 +0100

    Add PATCH and DELETE request

--- backend/src/movies/movies.controller.ts
+++ backend/src/movies/movies.controller.ts
@@ -2,12 +2,15 @@ import {
   Controller,
   Get,
   Post,
+  Patch,
+  Delete,
   Body,
   Param,
   ParseIntPipe,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('movies')
@@ -28,5 +31,18 @@ export class MoviesController {
   @Post()
   create(@Body() createMovieDto: CreateMovieDto) {
     return this.moviesService.create(createMovieDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateMovieDto: UpdateMovieDto,
+  ) {
+    return this.moviesService.update(id, updateMovieDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.moviesService.remove(id);
   }
 }
