8b477381a437c0c2f242a41f73f7203b7a94644b
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-03-15 15:21:19 +0100

    Add PATCH and DELETE request logic

--- backend/src/movies/movies.service.ts
+++ backend/src/movies/movies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Injectable()
 export class MoviesService {
@@ -31,6 +32,29 @@ export class MoviesService {
         },
       },
       include: { genres: true },
+    });
+  }
+
+  update(id: number, dto: UpdateMovieDto) {
+    return this.prisma.movie.update({
+      where: { id },
+      data: {
+        title: dto.title,
+        year: dto.year,
+        rating: dto.rating,
+        plot: dto.plot,
+        posterUrl: dto.posterUrl,
+        genres: dto.genreIds
+          ? { set: dto.genreIds.map((id) => ({ id })) }
+          : undefined,
+      },
+      include: { genres: true },
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.movie.delete({
+      where: { id },
     });
   }
 }
