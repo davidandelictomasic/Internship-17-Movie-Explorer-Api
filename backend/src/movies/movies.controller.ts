7ebca4224dfaa7efc7b1c6247ebdb822537e6fcb
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-03-15 11:59:40 +0100

    Add GET and POST requests

--- backend/src/movies/movies.controller.ts
+++ backend/src/movies/movies.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('movies')
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  findAll() {
+    return this.moviesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.moviesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createMovieDto: CreateMovieDto) {
+    return this.moviesService.create(createMovieDto);
+  }
+}
