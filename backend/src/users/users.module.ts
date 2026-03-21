8629cb082d0530be5ab18f8d1c5458aec1071078
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-03-21 01:29:35 +0100

    Add JWT strategy for token validation

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, JwtStrategy],
   exports: [UsersService],
 })
 export class UsersModule {}
